a481b91701a4db6bd472a09cb24d10ec35a539a1
jpvolt <jpvoltdeveloper@gmail.com>
2019-06-02 17:51:42 -0300

    added vcu speed. not working

--- src/car.cpp
+++ src/car.cpp
@@ -23,10 +23,11 @@ lin::Mat<double> F(lin::Mat<double> x, lin::Mat<double> u){
 }
 
 lin::Mat<double> G(lin::Mat<double> x){
-    lin::Mat<double> out(3,1);
+    lin::Mat<double> out(4,1);
     out(0,0) = x(0,0); // gpsx = x
     out(1,0) = x(1,0); // gpsy = y
     out(2,0) = x(2,0); // vpir = v
+    out(3,0) = x(2,0); // vvcu = v
     return out;
 }
 lin::Mat<double> GPStoCart(double latitude, double longitude){
@@ -42,16 +43,17 @@ int main(int argc, char *argv[]){
     lin::Mat<double> X(4,1);
     X = 0;
 
-    lin::Mat<double> R(3,3);
+    lin::Mat<double> R(4,4);
     R = 0;
     R(0,0) = 3.3; // gpsx error
     R(1,1) = 3.3; // gpsy error
     R(2,2) = 2; // vpir error
+    R(3,3) = 100; // vcu error
 
     lin::Mat<double> Q(4,4);
-    Q = Q.I()*0.1;
+    Q = Q.I();
 
-    lin::Mat<double> Z(3,1);
+    lin::Mat<double> Z(4,1);
     Z = 0;
 
     lin::Mat<double> control(3,1);
@@ -67,7 +69,7 @@ int main(int argc, char *argv[]){
     ekf.setControl(&control);
     ekf.setF(F);
     ekf.setG(G);
-    ekf.init(1);
+    ekf.init(100);
 
 
     // load gps csv
@@ -97,15 +99,28 @@ int main(int argc, char *argv[]){
     // compute pir sensor frequency from time data
     double pir_freq = timePir.size()/(timePir.back()-timePir[0]);
 
-    int index_gps, index_pir, index_steer;
+
+    // load vcu csv 
+    rapidcsv::Document vcuCSV("Final_Log_VCU_Info_1.csv", rapidcsv::LabelParams(0,-1), rapidcsv::SeparatorParams(';'),rapidcsv::ConverterParams(true));
+    // load pir data into vectors
+    std::vector<double> vcu = vcuCSV.GetColumn<double>("Speed (VCU) [km/h]");
+    std::vector<double> timeVcu  = vcuCSV.GetColumn<double>("Time [s]");
+    // compute pir sensor frequency from time data
+    double vcu_freq = timeVcu.size()/(timeVcu.back()-timeVcu[0]);
+
+    int index_gps, index_pir, index_steer, index_vcu;
+
+    int log_ofset = 31000;
+
+    index_gps = (gps_freq/pir_freq)*log_ofset;
 
     // initialize X 
     lin::Mat<double> cart;
-    cart = GPStoCart(latitude[0], longitude[0]);
+    cart = GPStoCart(latitude[index_gps], longitude[index_gps]);
     X(0,0) = cart(0,0); 
     X(1,0) = cart(1,0); 
 
-    cart = GPStoCart(latitude[100], longitude[100]);
+    cart = GPStoCart(latitude[index_gps + 5], longitude[index_gps + 5]);
     double dx, dy;
     dx = cart(0,0) - X(0,0); 
     dy = cart(1,0) - X(1,0); 
@@ -114,11 +129,12 @@ int main(int argc, char *argv[]){
     X(2,0) = pir[0];
 
 
-    std::vector<double> gpsx, gpsy,  pirv,  x, y, v, theta;
+    std::vector<double> gpsx, gpsy,  pirv,vcuv,  x, y, v, theta;
 
-    for(int i = 31000;i<33000;i++){
+    for(int i = log_ofset;i<33000;i++){
         index_gps = (gps_freq/pir_freq)*i;
         index_steer = (steer_freq/pir_freq)*i;
+        index_vcu = (vcu_freq/pir_freq)*i;
         index_pir = i;
 
         cart = GPStoCart(latitude[index_gps], longitude[index_gps]);
@@ -128,6 +144,8 @@ int main(int argc, char *argv[]){
         gpsy.push_back(Z(1,0));
         Z(2,0) = pir[index_pir];
         pirv.push_back(Z(2,0));
+        //Z(3,0) = vcu[index_vcu];
+        vcuv.push_back(Z(3,0));
 
         control(1,0) = steering[index_steer] * 0.017 ; // multiply by 0.017 to convert from degres to radians
 
@@ -141,12 +159,15 @@ int main(int argc, char *argv[]){
     }
 
 
-    plt::named_plot("Gps x", gpsx);
-    plt::named_plot("kalman x",x);
-    //plt::named_plot("pir v", pirv);
-    //plt::named_plot("kalman v",v);
+    //plt::named_plot("Gps x", gpsx);
+    //plt::named_plot("kalman x",x);
+    plt::named_plot("pir v", pirv);
+    plt::named_plot("vcu v", vcuv);
+    plt::named_plot("kalman v",v);
     plt::legend();
     plt::show();
 
 }
+
+
 
